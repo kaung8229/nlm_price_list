587b4191f37c0d4ae644aabf2a0f38bcdb4b068b
Kaung Myat <kaung8229@gmail.com>
2023-12-28 11:43:42 +0630

    lunch time

--- app.js
+++ app.js
@@ -5,6 +5,29 @@ let btnsubmit = document.querySelector('.btn-submit');
 
 let profileuser = document.querySelector('.profile');
 
+let loginpage = document.querySelector('.loginpage');
+let accesspage = document.querySelector('.accesspage');
+let logout = document.querySelector('.logout');
+
+
+// go back to login page when logout
+logout.addEventListener('click',function(){
+    loginpage.classList.remove("dnone");
+    accesspage.classList.add("dnone");
+
+    // remove access after logout
+    localStorage.setItem("access",0);
+})
+
+
+// check access
+let getaccess = localStorage.getItem("access");
+// console.log(getaccess);
+if(getaccess == "1"){
+    loginpage.classList.add("dnone");
+    accesspage.classList.remove("dnone");
+}
+
 
 // remove error input field style
 function removeerror(e){
@@ -13,14 +36,19 @@ function removeerror(e){
 }
 
 btnsubmit.addEventListener('click',function(e){
+    e.preventDefault(); // stop form loading
 
     let username = usernameui.value;
     let password = passwordui.value;
 
     if(username.trim() != '' && password.trim() != ''){ // check field isn't to be empty
-        console.log("Access");
+        loginpage.classList.add("dnone");
+        accesspage.classList.remove("dnone");
+
+        // after login giving access all the time until browser history clear
+        localStorage.setItem("access",1);
+
     }else{
-        e.preventDefault(); // stop form loading when error
 
         if(username.trim() == ''){
             usernameui.nextElementSibling.style.display = 'block';
@@ -39,3 +67,11 @@ btnsubmit.addEventListener('click',function(e){
 profileuser.addEventListener('click',function(){
     this.classList.toggle("active");
 })
+
+// close logout menu
+window.addEventListener('click',function(e){
+    // console.log(e.target);
+    if(e.target != profileuser){
+        profileuser.classList.remove("active");
+    }
+},true)
